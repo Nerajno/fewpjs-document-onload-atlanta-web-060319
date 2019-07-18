11c16924a82da2f3e9cd578bd7835fb9e25e26be
Nerajno <Nerajno@gmail.com>
2019-07-18 12:31:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+let newText = document.querySelector("#text")
+
+document.addEventListener("DOMContentLoaded", function() {
+    //let currentText = document.getElementById("text").innerText
+    newText.innerHTML ="This is really cool!";
+  });
